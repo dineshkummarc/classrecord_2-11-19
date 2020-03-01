8360bcda90608ce9354282a2081da784a7210c19
rockethyperlocal <mapmydestination@gmail.com>
2020-03-01 22:26:31 +0530

    first commit

---
+++ admin/footer.php
@@ -0,0 +1,9 @@
+
+
+
+ <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 col-xl-12">
+
+                        <p class="text-xs-center tm-footer-text"> Copyright &copy; <?php echo date ("y"); echo "Student of  St. Nicolas College";?></p>
+                        
+   </div>
+					
